00b7c8716219879f8c041413c26a73cc5a4909ad
Chris Mutel <cmutel@gmail.com>
2019-03-27 12:19:11 +0100

    Add units

--- bontofrom/load_metadata.py
+++ bontofrom/load_metadata.py
@@ -16,3 +16,9 @@ with open(metadata_dir / "exiobase_flowobject_URIs.csv", "r", encoding='utf-8') 
     # Skip header
     next(reader)
     metadata['flow objects'] = {row[0]: row[3] for row in reader if row}
+
+metadata['units'] = {
+    'kilogram': 'http://www.ontology-of-units-of-measure.org/resource/om-2/kilogram',
+    'megajoule': 'http://www.ontology-of-units-of-measure.org/resource/om-2/megajoule',
+    'euro': 'http://www.ontology-of-units-of-measure.org/resource/om-2/euro',
+}
