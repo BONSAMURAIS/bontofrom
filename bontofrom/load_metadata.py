e19eb4c3c9e064d75bc4f6a85882365ded3d7b92
Chris Mutel <cmutel@gmail.com>
2019-03-27 13:10:49 +0100

    Update metadata load

--- bontofrom/load_metadata.py
+++ bontofrom/load_metadata.py
@@ -5,7 +5,7 @@ metadata_dir = Path(__file__).parent / "meta"
 
 metadata = {}
 
-with open(metadata_dir / "exiobase_activity_URIs.csv", "r", encoding='utf-8') as f:
+with open(metadata_dir / "exiobase_activitytype_URIs.csv", "r", encoding='utf-8') as f:
     reader = csv.reader(f)
     # Skip header
     next(reader)
@@ -16,6 +16,15 @@ with open(metadata_dir / "exiobase_flowobject_URIs.csv", "r", encoding='utf-8') 
     # Skip header
     next(reader)
     metadata['flow objects'] = {row[0]: row[3] for row in reader if row}
+
+get_location = lambda l: (l[0], l[1]) if l[1] else (l[0], l[2])
+
+with open(metadata_dir / "exiobase_location_URIs.csv", "r", encoding='utf-8') as f:
+    reader = csv.reader(f)
+    # Skip header
+    next(reader)
+    metadata['locations'] = dict(get_location(row) for row in reader if row)
+
 
 metadata['units'] = {
     'kilogram': 'http://www.ontology-of-units-of-measure.org/resource/om-2/kilogram',
