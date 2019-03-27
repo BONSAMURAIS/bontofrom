0b35cbb3527653cca29c2de592935b2805318ac0
Chris Mutel <cmutel@gmail.com>
2019-03-27 12:17:14 +0100

    Be more technically correct
    It is the best kind of correct

--- bontofrom/load_metadata.py
+++ bontofrom/load_metadata.py
@@ -9,7 +9,7 @@ with open(metadata_dir / "exiobase_activity_URIs.csv", "r", encoding='utf-8') as
     reader = csv.reader(f)
     # Skip header
     next(reader)
-    metadata['activities'] = {row[0]: row[3] for row in reader if row}
+    metadata['activity type'] = {row[0]: row[3] for row in reader if row}
 
 with open(metadata_dir / "exiobase_flowobject_URIs.csv", "r", encoding='utf-8') as f:
     reader = csv.reader(f)
