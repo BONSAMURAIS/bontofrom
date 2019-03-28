f64deb377165e3b0008219b85c20692af66cfd0c
Chris Mutel <cmutel@gmail.com>
2019-03-28 09:42:55 +0100

    Add first draft of exiobase data header

---
+++ bontofrom/exiobase_header.py
@@ -0,0 +1,12 @@
+HEADER = {
+    "@context": {
+        "brdfat": "http://rdf.bonsai.uno/activitytype/exiobase3_3_17/",
+        "brdffo": "http://rdf.bonsai.uno/flowobject/exiobase3_3_17/",
+        "brdfl": "http://rdf.bonsai.uno/location/exiobase3_3_17/",
+        "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+        "bont": "http://ontology.bonsai.uno/core#",
+        "om2": "http://www.ontology-of-units-of-measure.org/resource/om-2/",
+        "geo": "http://sws.geonames.org/",
+        "schema":  "http://schema.org/",
+    }
+}
