934045aa013b7ff54feb15dcdfb0751555245ff5
Chris Mutel <cmutel@gmail.com>
2019-03-28 11:08:53 +0100

    Add time

--- bontofrom/convert_metadata.py
+++ bontofrom/convert_metadata.py
@@ -14,6 +14,7 @@ Please run the following to convert JSON-LD to TTL:
     docker run -it --rm -v `pwd`:/rdf stain/jena riot -out Turtle bontofrom/output/activitytype.jsonld > bontofrom/output/activitytype.ttl
     docker run -it --rm -v `pwd`:/rdf stain/jena riot -out Turtle bontofrom/output/location.jsonld > bontofrom/output/location.ttl
     docker run -it --rm -v `pwd`:/rdf stain/jena riot -out Turtle bontofrom/output/unit.jsonld > bontofrom/output/unit.ttl
+    docker run -it --rm -v `pwd`:/rdf stain/jena riot -out Turtle bontofrom/output/time.jsonld > bontofrom/output/time.ttl
 """.format((Path(__file__).parent.parent).absolute())
 
 
@@ -44,7 +45,7 @@ class Converter:
         for name, uri in self.metadata[self.filename].items():
             data['@graph'].append({
                 '@id': self.substitute(uri),
-                "@type" : self.type_,
+                "@type": self.type_,
                 "rdfs:label": name,
             })
         return data
@@ -54,6 +55,80 @@ class Converter:
             json.dump(self.get_data(), f,
                       ensure_ascii=False, indent=2)
 
+
+class ProperInterval(Converter):
+    def __init__(self, years):
+        """``years`` is a list of integer years."""
+        self.years = years
+        self.filename = "time"
+
+    def map_wikidata_year(self, year):
+        MAPPING = {
+            2011: "https://www.wikidata.org/wiki/Q1994",
+            2015: "https://www.wikidata.org/wiki/Q2002",
+            2016: "https://www.wikidata.org/wiki/Q25245",
+            2017: "https://www.wikidata.org/wiki/Q25290",
+            2018: "https://www.wikidata.org/wiki/Q25291",
+        }
+        return MAPPING[year]
+
+    def get_data(self):
+        data = {
+            "@context" : {
+                "years" : {
+                    "@id" : "https://www.w3.org/TR/owl-time/years",
+                    "@type" : "http://www.w3.org/2001/XMLSchema#integer"
+                },
+                "sameAs" : {
+                    "@id" : "http://www.w3.org/2002/07/owl#sameAs",
+                    "@type" : "@id"
+                },
+                "hasEnd" : {
+                    "@id" : "https://www.w3.org/TR/owl-time/hasEnd",
+                    "@type" : "@id"
+                },
+                "hasDurationDescription" : {
+                    "@id" : "https://www.w3.org/TR/owl-time/hasDurationDescription",
+                "@type" : "@id"
+                },
+                "hasBeginning" : {
+                    "@id" : "https://www.w3.org/TR/owl-time/hasBeginning",
+                    "@type" : "@id"
+                },
+                "inXSDDate" : {
+                    "@id" : "https://www.w3.org/TR/owl-time/inXSDDate",
+                    "@type" : "http://www.w3.org/2001/XMLSchema#date"
+                },
+                "brdftim" : "http://rdf.bonsai.uno/time/",
+                "time" : "https://www.w3.org/TR/owl-time/",
+                },
+            "@graph": [{
+                "@id" : "brdftim:oneyearlong",
+                "@type" : "time:DurationDescription",
+                "time:years" : 1
+            }]
+        }
+        for year in self.years:
+            data['@graph'].extend([{
+                "@id": "brdftim:{}end".format(year),
+                "@type": "time:Instant",
+                "inXSDDate": "{}-12-31".format(year),
+            }, {
+                "@id": "brdftim:{}start".format(year),
+                "@type": "time:Instant",
+                "inXSDDate": "{}-01-01".format(year),
+            }, {
+                "@id": "brdftim:{}".format(year),
+                "@type": "time:ProperInterval",
+                "sameAs": [
+                    self.map_wikidata_year(year),
+                    "http://reference.data.gov.uk/doc/year/{}".format(year)
+                ],
+                "hasBeginning": "brdftim:{}start".format(year),
+                "hasDurationDescription": "brdftim:oneyearlong",
+                "hasEnd": "brdftim:{}end".format(year),
+            }])
+        return data
 
 
 def convert_exiobase():
@@ -94,6 +169,9 @@ def convert_exiobase():
         metadata,
     )
     unit.write_file()
+
+    time = ProperInterval([2011, 2016, 2017, 2018])
+    time.write_file()
 
     print(EXIOBASE_DOCKER)
     pass
--- bontofrom/exiobase_header.py
+++ bontofrom/exiobase_header.py
@@ -4,6 +4,7 @@ HEADER = {
         "brdffo": "http://rdf.bonsai.uno/flowobject/exiobase3_3_17/",
         "brdfl": "http://rdf.bonsai.uno/location/exiobase3_3_17/",
         "brdfsu": "http://rdf.bonsai.uno/sutdata/exiobase3_3_17/",
+        "brdftim": "http://rdf.bonsai.uno/time/exiobase3_3_17/",
         "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
         "bont": "http://ontology.bonsai.uno/core#",
         "om2": "http://www.ontology-of-units-of-measure.org/resource/om-2/",
